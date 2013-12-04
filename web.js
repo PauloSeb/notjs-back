d4091e24981aeddd99ca49752672a8285671e1b2
PauloSeb <paul.sebastien974@gmail.com>
2013-12-04 17:39:26 +0100

    Websocket helloworld

--- web.js
+++ web.js
@@ -1,12 +1,27 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var WebSocketServer = require('ws').Server
+  , http = require('http')
+  , express = require('express')
+  , app = express()
+  , port = process.env.PORT || 5000;
 
-app.get('/', function(request, response) {
-  response.send("Hello world");
-});
+app.use(express.static(__dirname + '/'));
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var server = http.createServer(app);
+server.listen(port);
+
+console.log('http server listening on %d', port);
+
+var wss = new WebSocketServer({server: server});
+console.log('websocket server created');
+wss.on('connection', function(ws) {
+    var id = setInterval(function() {
+        ws.send(JSON.stringify(new Date()), function() {  });
+    }, 1000);
+
+    console.log('websocket connection open');
+
+    ws.on('close', function() {
+        console.log('websocket connection close');
+        clearInterval(id);
+    });
 });
